d6fb9b904db934f424f4f5db7a97ba0421d78264
Karepanova <89221006233@yandex.ru>
2021-05-23 01:43:40 +0500

    Итог 3

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,11 @@ let editButton = content.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseIcon = popup.querySelector('.popup__close-icon');
 let formElement = document.querySelector('.popup__data');
-let popupInfoName = formElement.querySelector('.popup__info_name');
-let popupInfoAbout = formElement.querySelector('.popup__info_about');
+let popupInfoName = formElement.querySelector('.popup__info_profile_name');
+let popupInfoAbout = formElement.querySelector('.popup__info_profile_about');
 let title = document.querySelector('.profile__title');// Выберите элементы, куда должны быть вставлены значения полей
 let subtitle = document.querySelector('.profile__subtitle');// Выберите элементы, куда должны быть вставлены значения полей
 
-editButton.addEventListener('click', openClick); /*ф-ция добавить класс*/
-popupCloseIcon.addEventListener('click', closeClick); /*ф-ция удалить класс*/
-formElement.addEventListener('submit', formSubmitHandler); /*ф-ция отправки формы*/
 
 function openClick() {
   popup.classList.add('popup_opened'); /*добавить модификатор*/
@@ -29,3 +26,7 @@ function formSubmitHandler (evt) {
   subtitle.textContent = popupInfoAbout.value;// Вставьте новые значения с помощью textContent
   closeClick();
 }
+
+editButton.addEventListener('click', openClick); /*ф-ция добавить класс*/
+popupCloseIcon.addEventListener('click', closeClick); /*ф-ция удалить класс*/
+formElement.addEventListener('submit', formSubmitHandler); /*ф-ция отправки формы*/
