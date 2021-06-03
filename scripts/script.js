c98f08a79f9949daca4b4272153871a2e3bcd427
Karepanova <89221006233@yandex.ru>
2021-06-04 01:03:05 +0500

    итог

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,16 @@ const editButton = content.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseIcon = popup.querySelector('.popup__close-icon');
 const formElement = document.querySelector('.popup__data');
-const formEmptyData = document.querySelector('.form-empty__data');
+const formAddCardData = document.querySelector('.form-add-card__data');
 const popupInfoName = formElement.querySelector('.popup__info_profile_name');
 const popupInfoAbout = formElement.querySelector('.popup__info_profile_about');
 const title = document.querySelector('.profile__title');// Выберите элементы, куда должны быть вставлены значения полей
 const subtitle = document.querySelector('.profile__subtitle');// Выберите элементы, куда должны быть вставлены значения полей
 const addButton = content.querySelector('.profile__add-button');
-const formEmpty = document.querySelector('.form-empty');
-const formEmptyCloseIcon = formEmpty.querySelector('.form-empty__close-icon');
-const profileNaming = document.querySelector('.form-empty__info_profile_naming'); //из инпута
-const profileLink = document.querySelector('.form-empty__info_profile_link');// из инпута
+const formAddCard = document.querySelector('.form-add-card');
+const formAddCardCloseIcon = formAddCard.querySelector('.form-add-card__close-icon');
+const profileNaming = document.querySelector('.form-add-card__info_profile_naming'); //из инпута
+const profileLink = document.querySelector('.form-add-card__info_profile_link');// из инпута
 const imagePopup = document.querySelector('.image-popup'); //попап картинка
 const imagePopupCloseIcon = document.querySelector('.image-popup__close-icon');//закрыть попап картинку
 const imagePopupImg = document.querySelector('.image-popup__img'); //поле ссылка попап фото
@@ -55,19 +55,19 @@ function openClick() {
 }
 
 /*добавляет класс отркрытия для формы "добавить фото"*/
-function openClickFormEmpty() {
-    formEmpty.classList.add('popup_opened');
-    profileNaming.value = ''; //чистит импут
-    profileLink.value = ''; //чистит импут
+function openClickFormAddCard() {
+    formAddCard.classList.add('popup_opened');
+    profileNaming.value = ''; //чистит инпут
+    profileLink.value = ''; //чистит инпут
 }
 
 /*добавляет класс отркрытия для попап фото*/
 function openClickImgPopup(event) {
     imagePopup.classList.add('popup_opened'); //добавляет класс для открытия
     imagePopupImg.src = event.target.src;  //записывает ссылку
-    imagePopupImg.alt = event.target.alt; //записывает альт
-    imagePopupTitle.textContent = event.target.alt; //записывает текст
-
+    let name = event.target.closest('.element').querySelector('.element__text').textContent; //ищем родителя, от родителя ищем текст
+    imagePopupImg.alt = name; //записывает альт
+    imagePopupTitle.textContent = name; //записывает текст
 }
 
 /*удаляет класс для "редактировать профиль"*/
@@ -76,8 +76,8 @@ function closeClick() {
 }
 
 /*удаляет класс для формы с добавлением фото*/
-function closeClickFormEmpty() {
-    formEmpty.classList.remove('popup_opened');
+function closeClickFormAddCard() {
+    formAddCard.classList.remove('popup_opened');
 }
 
 /*удаляет класс для фото попап*/
@@ -93,7 +93,6 @@ function formSubmitHandler(evt) {
     closeClick();
 }
 
-
 function formSubmitHandlerForm(evt) {
     evt.preventDefault();//не отправлять форму
     const element = {
@@ -101,13 +100,7 @@ function formSubmitHandlerForm(evt) {
         link: profileLink.value
     };
     renderItem(element);
-    closeClickFormEmpty();
-}
-
-/*обрабатывает создание всех карточек из массива*/
-function renderItems() {
-    initialCards.forEach(renderItem); //применяется к каждому элементу массива
-
+    closeClickFormAddCard();
 }
 
 /*создает из шаблона карточки с данными из массива*/
@@ -116,17 +109,14 @@ function renderItem(element) {
     initialElement.querySelector('.element__text').textContent = element.name; /*тянет в клон текст*/
     initialElement.querySelector('.element__img').src = element.link; /*тянет в клон ссылку*/
     initialElement.querySelector('.element__img').alt = element.name;
-
     setEventListeners(initialElement); //вызфвает ф-цию с обработчиками событий
     elementsList.prepend(initialElement); //вставляет клонируемые карточки в блок в начало
-
-
 }
-renderItems();
 
-
-
-
+/*обрабатывает создание всех карточек из массива*/
+(function renderItems() {
+    initialCards.forEach(renderItem); //применяется к каждому элементу массива
+})() /*самовызывающаяся функция*/
 
 /*удаляет карточку*/
 function handleDelete(event) {
@@ -138,7 +128,7 @@ function handleLike(event) {
     event.target.classList.toggle('element__button-active');
 }
 
-/*обработчик событий*/
+/*назначение событий*/
 function setEventListeners(element) {
     /*нажатие корзинка удалить вызывается ф-ция удаления блока*/
     element.querySelector('.profile__delete').addEventListener('click', handleDelete);
@@ -148,13 +138,11 @@ function setEventListeners(element) {
     element.querySelector('.element__img').addEventListener('click', openClickImgPopup);
 }
 
-
-
+/*обработчик событий*/
 editButton.addEventListener('click', openClick); /*ф-ция добавить класс*/
-addButton.addEventListener('click', openClickFormEmpty); /*ф-ция добавить класс для формы с фото*/
+addButton.addEventListener('click', openClickFormAddCard); /*ф-ция добавить класс для формы с фото*/
 popupCloseIcon.addEventListener('click', closeClick); /*ф-ция удалить класс*/
-formEmptyCloseIcon.addEventListener('click', closeClickFormEmpty); /*ф-ция удалить класс для формы с фото*/
+formAddCardCloseIcon.addEventListener('click', closeClickFormAddCard); /*ф-ция удалить класс для формы с фото*/
 imagePopupCloseIcon.addEventListener('click', closeClickImgPopup); /*ф-ция удалить класс для формы с фото*/
 formElement.addEventListener('submit', formSubmitHandler); /*ф-ция отправки формы*/
-formEmptyData.addEventListener('submit', formSubmitHandlerForm); /*ф-ция отправки формы*/
-
+formAddCardData.addEventListener('submit', formSubmitHandlerForm); /*ф-ция отправки формы*/
